b4e0d4ffaae7ad6b4abae189780c47afe66233bc
Nithishphoenix <98878162+Nithishphoenix@users.noreply.github.com>
2022-03-03 15:26:24 +0530

    Update Contact Management System.c

--- 3_Implementation/Contact Management System.c
+++ 3_Implementation/Contact Management System.c
@@ -2,126 +2,204 @@
 #include"Contact Management System.h"
 struct contact
 {
-    long ph;
-    char name[20],add[20],email[30];
-} list;
-char query[20],name[20];
-FILE *fp, *ft;
-int a,n,ch,l,found;
+	char name[100];
+	char mobileno[100];
+	char email[200];
+};
+struct contact s[100],temp;
+void addition();
+void validate();
+void deletion();
+void search();
+void display();
+void sort();
+int choice,i,count=0,j=0,k=0,m,n,c=0;
+char temp_name[100],temp_mobile[100],temp_email[100];
+char empty[100]="";
 int main()
 {
-main:
-    system("cls");
-    printf("\n\t Contact Management System");
-    printf("\n\n\n\t\tContact Diary\n\t\t\n\t[1] Add a new Contact No\n\t[2] List all Contacts\n\t[3] Delete a Contact \n\t[4] Search for contact\n\t[0] Exit\n\t");
-    printf("choose your preferred choice:");
-    scanf("%d",&ch);
-    switch(ch)
-    {
-    case 0:
-        printf("\n\n\t\tAre you sure you want to exit?");
-        break;
-    case 1:
-        system("cls");
-        fp=fopen("contact.dll","a");
-        for (;;)
-        {
-            fflush(stdin);
-            printf(" Enter the Name:");
-            scanf("%[^\n]",&list.name);
-            if(stricmp(list.name,"")==0 || stricmp(list.name," ")==0)
-                break;
-            fflush(stdin);
-            printf("Phone:");
-            scanf("%ld",&list.ph);
-            fflush(stdin);
-            printf("email address:");
-            gets(list.email);
-            printf("\n");
-            fwrite(&list,sizeof(list),1,fp);
-        }
-        fclose(fp);
-        break;
-    case 2:
-        system("cls");
-        printf("\n\t\t\n\t\t\tLIST OF CONTACTS\n\t\n");
-        for(a=97; a<=122; a=a+1)
-        {
-            fp=fopen("contact.dll","r");
-            fflush(stdin);
-            found=0;
-            while(fread(&list,sizeof(list),1,fp)==1)
-            {
-                if(list.name[0]==a || list.name[0]==a-32)
-                {
-                    printf("\nName\t %s\nPhone\t %ld%s\nEmail\t %s\n",list.name,
-                           list.ph,list.add,list.email);
-                    found++;
-                }
-            }
-            if(found!=0)
-            {
-                printf("[%c]-(%d)\n\n",a-32,found);
-                getch();
-            }
-            fclose(fp);
-        }
-        break;
-    case 3:
-        system("cls");
-        fflush(stdin);
-        printf("\n\n\tEnter the name of contact to be removed from your diary :");
-        scanf("%[^\n]",&name);
-        fp=fopen("contact.dll","r");
-        ft=fopen("temp.dat","w");
-        while(fread(&list,sizeof(list),1,fp)!=0)
-            if (stricmp(name,list.name)!=0)
-                fwrite(&list,sizeof(list),1,ft);
-        fclose(fp);
-        fclose(ft);
-        remove("contact.dll");
-        rename("temp.dat","contact.dll");
-    break;
-    case 4:
-        system("cls");
-        do
-        {
-            found=0;
-            printf("\n\n\tCONTACT SEARCH\n\t\n\tName of contact to search: ");
-            fflush(stdin);
-            scanf("%[^\n]",&query);
-            l=strlen(query);
-            fp=fopen("contact.dll","r");
-            system("cls");
-            printf("\n\nSearch result for '%s' \n\n",query);
-            while(fread(&list,sizeof(list),1,fp)==1)
-            {
-                for(a=0; a<=l; a++)
-                    name[a]=list.name[a];
-                name[l]='\0';
-                if(stricmp(name,query)==0)
-                {
-                    printf("\nName\t %s\nPhone\t %ld\n%s\nEmail\t%s\n",list.name,list.ph,list.add);
-                    found++;
-                    if (found%4==0)
-                    {
-                        printf("Press any key to continue");
-                        getch();
-                    }
-                }
-            }
-            if(found==0)
-                printf("\nNo match found!");
-            else
-                printf("\n%d match(s) found!",found);
-            fclose(fp);
-            printf("\nTry again\n\n\t[1] Yes\t\t[0] No\n\t");
-            scanf("%d",&ch);
-        }
-        while(ch==1);
-        break;
-default:
-        printf("Invalid option");
-    }
-    return 0;
+	while(1)
+	{
+		printf("\nCONTACT MANAGEMENT SYSTEM\n1) ADDITION\n2) DELETION\n3) SEARCH\n4) LIST CONTACTS \n5) EXIT\nEnter Your Choice:");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+			{
+				addition();
+				break;
+			}
+			case 2:
+			{
+				deletion();
+				break;
+			}
+			case 3:
+			{
+				search();
+				break;
+			}
+			case 4:
+			{
+				display();
+				break;
+			}
+			case 5:
+			{
+				exit(1);
+				break;
+			}
+			default:
+			{
+				printf("\nPlease Enter the valide choice:\n");
+				break;
+			}
+		}
+	}
+}
+void validate()
+{
+	for(i=0;i<j;i++)
+	{
+		if(!strcmp(s[i].name,temp_name))
+		{
+			printf("Name already existed!\n");
+			exit(1);
+		}
+	}
+	if(i==j)
+	{
+		count=0;
+		for(i=0;temp_email[i]!='\0';i++)
+		{
+			if(temp_email[i]=='@' || temp_email[i]=='.')
+			{
+				count++;
+			}
+		}
+		if(count!=2)
+		{
+			printf("Please Enter correct Email:\n");
+			exit(1);
+		}
+		else
+		{
+			if(!(strlen(temp_email)>=5))
+			{
+				printf("\nPlease Enter correct Email\n");
+				exit(1);
+			}
+		}
+	}
+	for(i=0;i<10;i++)
+	{
+		if(strlen(temp_mobile)!=10 || temp_mobile[0]=='0')
+		{
+			printf("Please Enter the valid Mobile Number");
+			exit(1);
+		}
+		else if(!(temp_mobile[i]>='0' && temp_mobile[i]<='9'))
+		{
+			printf("Please Enter the valid Mobile Number:");
+			exit(1);
+		}
+	}
+	for(i=0;i<j;i++)
+	{
+		if(!strcmp(temp_mobile,s[i].mobileno))
+		{
+			printf("Phone Number already existed in contact book\n");
+			exit(1);
+		}
+	}
+}
+void addition()
+{
+	printf("Enter the Name:");
+	scanf("%s",temp_name);
+	printf("Enter the Mobile Number:");
+	scanf("%s",&temp_mobile);
+	printf("Enter the email:");
+	scanf("%s",temp_email);
+	validate();
+	strcpy(s[j].name,temp_name);
+	strcpy(s[j].mobileno,temp_mobile);
+	strcpy(s[j].email,temp_email);
+	j++;
+	c++;
+	printf("\n\nContact saved successfully\n");
+}
+void deletion()
+{
+	printf("Enter the Name:\t");
+	scanf("%s",temp_name);
+	for(i=0;i<j;i++)
+	{
+		if(!strcmp(s[i].name,temp_name))
+		{
+			strcpy(s[i].name,empty);
+			strcpy(s[i].mobileno,empty);
+			strcpy(s[i].email,empty);
+			printf("\nContact Successfully Deleted!\n");
+			c--;
+			break;
+		}
+	}
+	if(i==j)
+	{
+		printf("\nContact Not found!\n");
+	}
+}
+void search()
+{
+	printf("Enter the Name:\t");
+	scanf("%s",temp_name);
+	for(i=0;i<j;i++)
+	{
+		if(!strcmp(s[i].name,temp_name))
+		{
+			printf("Name : %s\nMobile No.: %s\nE-Mail : %s\n",s[i].name,s[i].mobileno,s[i].email);
+			break;
+		}
+	}
+	if(i==j)
+	{
+		printf("\n Contact Not Found\n");
+	}
+}
+void sort()
+{
+	for(m=1;m<j;m++)
+	{
+		for(n=0;n<j-m;n++)
+		{
+			if(strcmp(s[n].name,s[n+1].name)>0)
+			{
+				temp=s[n];
+				s[n]=s[n+1];
+				s[n+1]=temp;
+			}
+		}
+	}
+}
+void display()
+{
+	if(c==0)
+	{
+		printf("\n\nNo Contact to displayed!!\n\n");
+	}
+	else
+	{
+		sort();
+		printf("\n\n-----------Your Contact Dairy is-------------\n\n");
+		for(i=0;i<j;i++)
+		{
+			if(strcmp(s[i].name,empty))
+			{
+				printf("Name : %s\nMobile No.: %s\nE-Mail : %s\n",s[i].name,s[i].mobileno,s[i].email);
+				printf("\n--------------\n");
+			}
+		}
+		printf("\n\n-------THE END--------\n\n");
+	}
 }
